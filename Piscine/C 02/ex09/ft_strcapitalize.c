1343f9c54100a8d85c8929c9e4f044736de2ae8d
Javier Dacal Alvarez <jdacal-a@c1r5s2.42urduliz.com>
2024-06-25 19:19:58 +0200

    Correccion C02 ex09

--- Piscine/C 02/ex09/ft_strcapitalize.c
+++ Piscine/C 02/ex09/ft_strcapitalize.c
@@ -6,9 +6,24 @@
 /*   By: jdacal-a <jdacal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 07:33:18 by jdacal-a          #+#    #+#             */
-/*   Updated: 2024/06/24 07:33:20 by jdacal-a         ###   ########.fr       */
+/*   Updated: 2024/06/25 19:19:13 by jdacal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_alphanumeric(char c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
 
 char	*ft_strcapitalize(char *str)
 {
@@ -21,17 +36,13 @@ char	*ft_strcapitalize(char *str)
 	while (str[i] != '\0')
 	{
 		f = str[i - 1];
-		if ((i == 0) && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] -= 32;
-		}
-		else if ((i > 0) && (f == ' ' || f == '-' || f == '+'))
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if (ft_str_is_alphanumeric(f) == 0)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] -= 32;
 		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
 		i++;
 	}
 	return (temp);
@@ -42,7 +53,7 @@ char	*ft_strcapitalize(char *str)
 char *ft_strcapitalize(char *str);
 
 int main(void) {
-    char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+    char str[] = "¿salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 
     printf("Original: %s\n", str);
     printf("Capitalized: %s\n", ft_strcapitalize(str));
