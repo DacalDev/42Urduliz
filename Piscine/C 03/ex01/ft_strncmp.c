924c1e7dfb29d8e71af524c3896d0cb8cc36700b
Javier Dacal <JavierDacalAlvarez@gmail.com>
2024-06-26 12:16:38 +0200

    Correccion de norma c03

--- Piscine/C 03/ex01/ft_strncmp.c
+++ Piscine/C 03/ex01/ft_strncmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-    
+	
 }
